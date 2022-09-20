7f4bd485079215147df65fba995b608f1ed9667e
camila-1237 <111140571+camila-1237@users.noreply.github.com>
2022-09-20 17:03:29 -0500

    trabajo de entrada y salidas 3

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     private final static double PI = 3.14;
-    private static 
+    private static int numero1;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("digite su primer numero");
         numero1 = input.nextInt();
-        System.out.println("resultado: " +(numero1*PI);
+        System.out.println("resultado: " +(numero1*PI));
     }
 }
