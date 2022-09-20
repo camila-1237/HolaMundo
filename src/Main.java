08df3e417eedc9cbc3b898e5b521f36574b378c9
camila-1237 <111140571+camila-1237@users.noreply.github.com>
2022-09-20 16:52:47 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private final static double PI = 3.14;
+    private static 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("digite su primer numero");
+        numero1 = input.nextInt();
+        System.out.println("resultado: " +(numero1*PI);
+    }
+}
